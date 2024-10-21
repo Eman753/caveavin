425b70d6cc91f9e7638d389a5fa260ac95716085
EmanZR <emerick@rtnet.fr>
2024-10-21 16:43:00 +0200

    Modifications de main

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 
 from cli import *
-from web import *
+from flask import Flask
 
-# Main script of caveavin
+# Script principal de caveavin
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Bienvenue sur caveavin !</h1>"
 
 if __name__ == "__main__":
     choice = str(input("Dans quel mode lancer l'application ? web/console -> "))
+    print("")
     if choice == "console":
         cli()
-    elif choice == web:
-        webapp()
+    elif choice == "web":
+        ip_address = str(input("Adresse IP du serveur -> "))
+        print("")
+        print("Activation du serveur web")
+        app.run(host=ip_address, port=5001)
     else:
         cli()
+
 
 exit(0)
